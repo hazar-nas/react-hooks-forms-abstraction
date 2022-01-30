c5d56371d79cd522a258a23cdf3112da57498a2f
hazar-nas <hazarcan.nas@gmail.com>
2022-01-30 21:31:20 +0300

    finished

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,52 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  function handleChange(event) {
+    // name is the KEY in of the formData object we're trying to update
+    // const name = event.target.name;
+    // const value = event.target.value;
+    const {name, value} = event.target;
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+    // use `checked` property of checkboxes instead of `value`
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+  
+    setFormData({
+      ...formData, // otherwise you'd lose the data of the lastName or lastName one is updated
+      [name]: value,
+    });
+  } 
+
+  
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
     </form>
   );
-}
+} 
 
 export default Form;
